e0c7f16a10b15e8f8ccbb382087e394c7d90b481
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-05-05 22:36:10 +0800

    Update Timer component to use YouTube stream time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Timer from "@/components/Timer";
-import { isMrPokkeStreaming } from "@/lib/actions";
+import { isYouTubeStreaming } from "@/lib/actions";
+
+export const revalidate = 0; // Disable revalidation or caching
 
 export default async function Home() {
-  return <Timer isLive={await isMrPokkeStreaming()} />;
+  return (
+    <>
+      <Timer isLive={await isYouTubeStreaming()} />
+    </>
+  );
 }
